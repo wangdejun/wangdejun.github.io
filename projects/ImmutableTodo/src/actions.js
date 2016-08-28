00d64e73d8557c8f2416e7c885852f0d9eb1ca1b
wangdejun <1074201799@qq.com>
2016-08-28 17:36:26 +0800

    add the Immutatble Redux ToDo List

---
+++ projects/ImmutableTodo/src/actions.js
@@ -0,0 +1,22 @@
+// src/actions.js
+
+// succinct hack for generating passable unique ids
+const uid = () => Math.random().toString(34).slice(2);
+
+export function addTodo(text) {
+  return {
+    type: 'ADD_TODO',
+    payload: {
+      id: uid(),
+      isDone: false,
+      text: text
+    }
+  };
+}
+
+export function toggleTodo(id) {
+  return {
+    type: 'TOGGLE_TODO',
+    payload: id
+  }
+}
